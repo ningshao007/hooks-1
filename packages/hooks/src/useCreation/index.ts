bf1ed623e56e908663d9562b954075e93ec97c01
lyn <linxiaosheng@wavegroup.cn>
2022-09-16 15:42:36 +0800

    chore: add oil

--- packages/hooks/src/useCreation/index.ts
+++ packages/hooks/src/useCreation/index.ts
@@ -3,15 +3,18 @@ import { useRef } from 'react';
 import depsAreSame from '../utils/depsAreSame';
 
 export default function useCreation<T>(factory: () => T, deps: DependencyList) {
+  // 使用useRef保持值不变,多次渲染过程中不变
   const { current } = useRef({
     deps,
     obj: undefined as undefined | T,
     initialized: false,
   });
+
   if (current.initialized === false || !depsAreSame(current.deps, deps)) {
     current.deps = deps;
     current.obj = factory();
     current.initialized = true;
   }
+
   return current.obj as T;
 }
